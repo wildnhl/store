24a1cd7e04a7ae4022bff469392ea8da1acb437e
wildnhl <minnesotanhlwild@gmail.com>
2024-04-04 16:50:26 +0300

    refactor(next): new next app + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard', 'standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    semi: 0,
+    '@typescript-eslint/semi': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'space-before-function-paren': 0,
+    '@typescript-eslint/space-before-function-paren': 0
+  }
+};
