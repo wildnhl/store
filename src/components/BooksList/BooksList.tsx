0cd44fb9f2b425e88d69506490e5aee15bb7b95a
wildnhl <minnesotanhlwild@gmail.com>
2024-07-29 20:01:25 +0300

    feat(pagination): insert pagination to boollist

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,14 +1,22 @@
 import { BookListItem } from '@/components/BookListItem/BookListItem';
 import { type IBookShort } from '@/types/newRelease';
 import style from './bookList.module.scss';
+import { Pagination } from '../Pagination/Pagination';
 interface IProps {
   books: IBookShort[];
+  totalBooks: string;
+  page?: string;
 }
 
-export async function BookList({ books }: IProps) {
+export async function BookList({ books, totalBooks, page }: IProps) {
+  // console.log('page', page);
+  const paginationShow = page != null && (
+    <Pagination total={totalBooks} currentPage={+page} />
+  );
   return (
     <>
       <section>
+        {paginationShow}
         <ul className={style.gridContainer}>
           {books.map(({ title, isbn13, price, image }) => {
             return (
@@ -22,7 +30,7 @@ export async function BookList({ books }: IProps) {
             );
           })}
         </ul>
-        <div>GG</div>
+        {paginationShow}
       </section>
     </>
   );
